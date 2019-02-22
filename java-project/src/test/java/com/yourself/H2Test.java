09333606e1b52fba861cbb0451cd3860f936f46c
Gaetan Eleouet <gaetan.eleouet@gmail.com>
2019-02-22 17:15:05 +0100

    asserts

--- java-project/src/test/java/com/yourself/H2Test.java
+++ java-project/src/test/java/com/yourself/H2Test.java
@@ -2,6 +2,8 @@ package com.yourself;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -14,6 +16,7 @@ import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import org.junit.Assert;
 import org.junit.Test;
 public class H2Test {
 
@@ -30,53 +33,63 @@ public class H2Test {
 		}
 		
 		void add(Object o)   {
-			String apply = mapper.apply(o);
+			String apply = format(o);
 			max = Math.max(max, apply.length());
 			values.add(apply);
 		}
+
+		private String format(Object o) {
+			try {
+				return mapper.apply(o);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
+	}
+	
+	static class ResultQuery {
+		int columnCount;
+		ColumnRes[] columns;
+		int nbRow = 0;
+		
 	}
 	
 	@Test
 	public void test() throws Exception {
-		Connection conn = DriverManager.getConnection("jdbc:h2:mem:", "sa", "");
+		String fileName = "universe.sql";
 
-		createDeals(conn);
+//		-- SQL
+//		SELECT NAME, isnull(sum(Price), 0) as TOTAL FROM DEAL
+//		left outer join price on deal.dealId = price.dealId
+//		group by Name;
+
+
+
+		Connection conn = DriverManager.getConnection("jdbc:h2:mem:", "sa", "");
+		//createDeals(conn);
 		createPrices(conn);
 		
-		
-//		ResultSet executeQuery = conn.prepareStatement("SELECT * FROM DEAL").executeQuery();
-		String result="";
-		FileInputStream fis = new FileInputStream("universe.sql");
-		try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fis))) {
-			result = bufferedReader.lines().collect(Collectors.joining("\n"));
-		}
-		ResultSet executeQuery = conn.prepareStatement(result).executeQuery();
-		int columnCount = executeQuery.getMetaData().getColumnCount();
-		
-		ColumnRes[] columns = new ColumnRes[columnCount];
-		
-		for (int i = 1; i <= columnCount; i++) {
-			String columnClassName = executeQuery.getMetaData().getColumnClassName(i);
-			
-			String columnName = executeQuery.getMetaData().getColumnName(i);
-			columns[i-1] = createResColumn(columnClassName, columnName);
-		}
-		
-		
-		int nbRow = 0;
-		while (executeQuery.next()) {
-			for (int i = 1; i <= columnCount; i++) {
-				columns[i-1].add(executeQuery.getObject(i));
-			}
-			nbRow ++;
-		}
+		String request = readInput(fileName);
 
-		System.out.println(format(columnCount, nbRow, columns));
+		ResultQuery rq = executeQuery(conn, request);
+		System.out.println(format(rq));
+
+		Assert.assertEquals("First column should be named 'name'", "name", rq.columns[0].name.toLowerCase());
+		Assert.assertEquals("Second column should be named 'total'", "total", rq.columns[1].name.toLowerCase());
+		Assert.assertEquals("Names should be sorted", "A", rq.columns[0].values.get(0));
+		Assert.assertEquals("Names should be sorted", "B", rq.columns[0].values.get(1));
+		Assert.assertEquals("Value for B should be '14'", "14", rq.columns[1].values.get(1));
+		Assert.assertEquals("Value for D should be displayed", "D", rq.columns[0].values.get(3));
+		Assert.assertEquals("Value for D should be '0'", "0", rq.columns[1].values.get(3));
+		
 		
 		conn.close();
 	}
 
-	private StringBuilder format(int columnCount, int rowCount, ColumnRes[] columns) {
+	private StringBuilder format(ResultQuery rq) {
+		int columnCount = rq.columnCount;
+		ColumnRes[] columns = rq.columns;
 		StringBuilder header = new StringBuilder();
 		StringBuilder line = new StringBuilder();
 		for (int i = 0; i< columnCount; i++) {
@@ -92,8 +105,8 @@ public class H2Test {
 		StringBuilder out = new StringBuilder();
 		out.append(header).append("\n");
 		out.append(line).append("\n");
-
-		for (int j = 0; j < rowCount; j++) {
+		
+		for (int j = 0; j < rq.nbRow; j++) {
 			for (int i = 0; i< columnCount; i++) {
 				if (i!=0) {
 					out.append('|');
@@ -106,7 +119,40 @@ public class H2Test {
 		return out;
 	}
 
-	
+	private ResultQuery executeQuery(Connection conn, String request) throws SQLException {
+		ResultQuery rq = new ResultQuery();
+		ResultSet executeQuery = conn.prepareStatement(request).executeQuery();
+		rq.columnCount = executeQuery.getMetaData().getColumnCount();
+		
+		rq.columns = new ColumnRes[rq.columnCount];
+		rq.nbRow = 0;
+		
+		for (int i = 1; i <= rq.columnCount; i++) {
+			String columnClassName = executeQuery.getMetaData().getColumnClassName(i);
+			
+			String columnName = executeQuery.getMetaData().getColumnName(i);
+			rq.columns[i-1] = createResColumn(columnClassName, columnName);
+		}
+		
+		
+		while (executeQuery.next()) {
+			for (int i = 1; i <= rq.columnCount; i++) {
+				rq.columns[i-1].add(executeQuery.getObject(i));
+			}
+			rq.nbRow ++;
+		}
+		return rq;
+	}
+
+	private String readInput(String fileName) throws FileNotFoundException, IOException {
+		String result="";
+		FileInputStream fis = new FileInputStream(fileName);
+		try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fis))) {
+			result = bufferedReader.lines().collect(Collectors.joining("\n"));
+		}
+		return result;
+	}
+
 	private final static String BLANK = "                                ";
 	private final static String LINE  = "--------------------------------";
 	
@@ -124,14 +170,14 @@ public class H2Test {
 			break;
 
 		default:
-			mapper = o -> o!= null ? o.toString() : "NULL";
+			mapper = o -> o.toString();
 			break;
 		}
-		return new ColumnRes(columnName, mapper);
+		return new ColumnRes(columnName, o-> o!= null ? mapper.apply(o) : "NULL");
 	}
 
-	private void createPrices(Connection conn) throws SQLException {
-		conn.prepareStatement(
+	private void createPrices(Connection conn) throws Exception {
+		/*conn.prepareStatement(
 				"CREATE TABLE PRICE ("
 						+ "  DEALID INT PRIMARY KEY,"
 						+ "  PRICE FLOAT"
@@ -142,22 +188,26 @@ public class H2Test {
 		conn.prepareStatement("INSERT INTO PRICE VALUES(3, 7)").execute();
 		conn.prepareStatement("INSERT INTO PRICE VALUES(4, 10)").execute();
 		conn.prepareStatement("INSERT INTO PRICE VALUES(5, 1.6)").execute();
-		conn.prepareStatement("INSERT INTO PRICE VALUES(6, 8)").execute();
+		conn.prepareStatement("INSERT INTO PRICE VALUES(6, 8)").execute();*/
+		
+		String readInput = readInput("priceAndDeals.sql");
+		conn.prepareCall(readInput).execute();
+		
 	}
 
-	private void createDeals(Connection conn) throws SQLException {
-		conn.prepareStatement(
-				"CREATE TABLE DEAL ("
-				+ "  DEALID INT PRIMARY KEY,"
-				+ "  NAME VARCHAR"
-				+ ");"
-						).execute();
-		conn.prepareStatement("INSERT INTO DEAL VALUES(1, 'A')").execute();
-		conn.prepareStatement("INSERT INTO DEAL VALUES(2, 'B')").execute();
-		conn.prepareStatement("INSERT INTO DEAL VALUES(3, 'C')").execute();
-		conn.prepareStatement("INSERT INTO DEAL VALUES(4, 'D')").execute();
-		conn.prepareStatement("INSERT INTO DEAL VALUES(5, 'E')").execute();
-		conn.prepareStatement("INSERT INTO DEAL VALUES(6, 'F')").execute();
-	}
+//	private void createDeals(Connection conn) throws SQLException {
+//		conn.prepareStatement(
+//				"CREATE TABLE DEAL ("
+//				+ "  DEALID INT PRIMARY KEY,"
+//				+ "  NAME VARCHAR"
+//				+ ");"
+//						).execute();
+//		conn.prepareStatement("INSERT INTO DEAL VALUES(1, 'A')").execute();
+//		conn.prepareStatement("INSERT INTO DEAL VALUES(2, 'B')").execute();
+//		conn.prepareStatement("INSERT INTO DEAL VALUES(3, 'C')").execute();
+//		conn.prepareStatement("INSERT INTO DEAL VALUES(4, 'D')").execute();
+//		conn.prepareStatement("INSERT INTO DEAL VALUES(5, 'E')").execute();
+//		conn.prepareStatement("INSERT INTO DEAL VALUES(6, 'F')").execute();
+//	}
 	
 }
